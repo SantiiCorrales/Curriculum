8e2b28ab27f8cc4ca3eff8f4b240a9716dfd1e3b
Eddy Santiago <eddysantiagogh@gmail.com>
2023-10-02 23:00:13 -0500

    feat: add collapsed lists

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,10 @@ const fechaFooter = document.getElementById("fecha")
 const fecha = new Date().getFullYear();
 
 fechaFooter.innerHTML = fecha;
+
+var coll = document.querySelector(".collapsible");
+var content = document.querySelector(".content");
+
+coll.addEventListener("click", function() {
+  content.classList.toggle("active");
+});
